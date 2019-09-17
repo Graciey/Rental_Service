e0f0a5493322817763a45db077fab30457d60aa0
Graciey <testing9426@gmail.com>
2019-09-16 17:20:14 -0700

    First Commit.

---
+++ src/Component/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const Home = () => {
+    return (
+        <div>
+            <p>Hello from Home</p>
+        </div>
+    )
+       
+    
+}
+
+export default Home;
